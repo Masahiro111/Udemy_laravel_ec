b5d8429c806ecd4ea81326ca92ed3f0ac5cd22c6
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-10-10 20:13:49 +0900

    add slot2

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,6 +1,14 @@
 <x-tests.app>
 
     <x-slot name="header">ヘッダー１の部分だよ(=ﾟωﾟ)ﾉ</x-slot>
+    コンポーネントテスト１
 
-    ここに入力１
+    <x-tests.card>
+        <x-slot name="title">
+            タイトル(=ﾟωﾟ)ﾉ
+        </x-slot>
+        <x-slot name="content">
+            コンテンツ(;´･ω･)
+        </x-slot>
+    </x-tests.card>
 </x-tests.app>
