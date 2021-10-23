ab0479d8ac6f821de1e2168176db30a32a94395c
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-10-23 23:26:21 +0900

    add upload method

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -7,6 +7,7 @@ use App\Models\Shop;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 
 class ShopController extends Controller
 {
@@ -50,7 +51,14 @@ class ShopController extends Controller
     {
         $imageFile = $request->image;
         if (!is_null($imageFile) && $imageFile->isValid()) {
-            Storage::putFile('public/shops', $imageFile);
+            // Storage::putFile('public/shops', $imageFile); //リサイズなし
+
+            $fileName = uniqid(rand() . '_');
+            $extension = $imageFile->extension();
+            $fileNameToStore = $fileName . '.' . $extension;
+            $resizedImage = Image::make($imageFile)->resize(1920, 1080)->encode();
+
+            Storage::put('public/' . $fileNameToStore, $resizedImage);
         }
 
         return redirect()->route('owner.shops.index');
