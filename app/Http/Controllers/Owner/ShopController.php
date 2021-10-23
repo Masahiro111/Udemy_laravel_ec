ee293270b30365db7a9dd2fd9bd754e17254d31f
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-10-24 03:11:43 +0900

    add upload file

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UploadImageRequest;
 use App\Models\Shop;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
+use App\Services\ImageService;
 
 class ShopController extends Controller
 {
@@ -47,18 +49,20 @@ class ShopController extends Controller
         return view('owner.shops.edit', compact('shop'));
     }
 
-    public function update(Request $request, $id)
+    public function update(UploadImageRequest $request, $id)
     {
         $imageFile = $request->image;
         if (!is_null($imageFile) && $imageFile->isValid()) {
             // Storage::putFile('public/shops', $imageFile); //リサイズなし
 
-            $fileName = uniqid(rand() . '_');
-            $extension = $imageFile->extension();
-            $fileNameToStore = $fileName . '.' . $extension;
-            $resizedImage = Image::make($imageFile)->resize(1920, 1080)->encode();
+            // $fileName = uniqid(rand() . '_');
+            // $extension = $imageFile->extension();
+            // $fileNameToStore = $fileName . '.' . $extension;
+            // $resizedImage = Image::make($imageFile)->resize(1920, 1080)->encode();
 
-            Storage::put('public/' . $fileNameToStore, $resizedImage);
+            // Storage::put('public/' . $fileNameToStore, $resizedImage);
+
+            $fileNameToStore = ImageService::upload($imageFile, 'shops');
         }
 
         return redirect()->route('owner.shops.index');
